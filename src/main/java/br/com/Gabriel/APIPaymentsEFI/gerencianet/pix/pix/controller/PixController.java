b17192618535a35efd45115a8c81ae18126bed26
Lucas Martins <lucassena@alunos.utfpr.edu.br>
2023-06-17 16:21:41 -0300

    qr code e copia e cola

--- src/main/java/br/com/Gabriel/APIPaymentsEFI/gerencianet/pix/pix/controller/PixController.java
+++ src/main/java/br/com/Gabriel/APIPaymentsEFI/gerencianet/pix/pix/controller/PixController.java
@@ -1,18 +1,29 @@
 package br.com.Gabriel.APIPaymentsEFI.gerencianet.pix.pix.controller;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import java.util.Base64;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.Gabriel.APIPaymentsEFI.gerencianet.pix.service.PixService;
 
@@ -28,9 +39,35 @@ public class PixController {
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping(value = "/copia-e-cola", produces = "application/json")
-    public ResponseEntity<List<List<String>>> generateCharge(@RequestBody Map<String, String> request) {
-        System.out.println(request.get("valor"));
+    public ResponseEntity<List<List<String>>> generateCharge(@RequestBody PixRequest request) {
         try {
+            double valor = request.getValor();
+
+            DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
+            DecimalFormat decimalFormat = new DecimalFormat("0.00", symbols);
+            String valorString = decimalFormat.format(valor);
+
+            List<String> response = pixService.generateCharge(valorString);
+            List<List<String>> responseList = Arrays.asList(response);
+            return ResponseEntity.ok(responseList);
+        } catch (Exception e) {
+            // error response
+            List<String> errorResponse = Arrays.asList("error");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Arrays.asList(errorResponse));
+        }
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping(value = "/copia-e-cola2", produces = "application/json")
+    public ResponseEntity<List<List<String>>> generateCharge2(@RequestBody Map<String, String> request) {
+        try {
+            // Convert the request map to JSON string
+            String jsonRequest = new ObjectMapper().writeValueAsString(request);
+
+            // Use ObjectMapper to validate the JSON
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.readTree(jsonRequest);
+
             String valor = request.get("valor");
 
             List<String> response = pixService.generateCharge(valor);
@@ -42,6 +79,27 @@ public class PixController {
             List<String> errorResponse = Arrays.asList("error");
 
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Arrays.asList(errorResponse));
+        }
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping(value = "/qrcode/{id}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<byte[]> generateQRCode(@PathVariable("id") String id) {
+        try {
+            String base64Image = pixService.generateQRCode(id);
+
+            // Convert the base64 image to byte array
+            byte[] imageBytes = Base64.getDecoder().decode(base64Image);
+
+            // Set the response headers
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_PNG);
+            headers.setContentLength(imageBytes.length);
+
+            return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
--- src/main/java/br/com/Gabriel/APIPaymentsEFI/gerencianet/pix/service/PixService.java
+++ src/main/java/br/com/Gabriel/APIPaymentsEFI/gerencianet/pix/service/PixService.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
@@ -17,42 +18,8 @@ import br.com.gerencianet.gnsdk.exceptions.GerencianetException;
 @Service
 public class PixService {
 
-    public JSONObject generateCharge2(String cpf, String nome, String chavePix, double valor,
-            String solicitacaoPagador) throws FileNotFoundException {
-        String campo1 = "Informação Adicional1 do PSP-Recebedor";
-        String campo2 = "Informação Adicional3 do PSP-Recebedor";
-        Credentials credentials = new Credentials();
-
-        JSONObject options = new JSONObject();
-        options.put("client_id", credentials.getClientId());
-        options.put("client_secret", credentials.getClientSecret());
-        options.put("certificate", credentials.getCertificate());
-        options.put("sandbox", credentials.isSandbox());
-
-        JSONObject body = new JSONObject();
-        body.put("calendario", new JSONObject().put("expiracao", 3600));
-        body.put("devedor", new JSONObject().put("cpf", cpf).put("nome", nome));
-        body.put("valor", new JSONObject().put("original", String.valueOf(valor)));
-        body.put("chave", "acbf2098-fc24-4e9d-ace8-6b99fd5dee24");
-        body.put("solicitacaoPagador", solicitacaoPagador);
-
-        JSONArray infoAdicionais = new JSONArray();
-        infoAdicionais.put(new JSONObject().put("nome", "Campo 1").put("valor", campo1));
-        infoAdicionais.put(new JSONObject().put("nome", "Campo 2").put("valor", campo2));
-        body.put("infoAdicionais", infoAdicionais);
-
-        try {
-            Gerencianet gn = new Gerencianet(options);
-            JSONObject response = gn.call("pixCreateImmediateCharge", new HashMap<String, String>(), body);
-            return response;
-        } catch (GerencianetException e) {
-            System.out.println(e.getError());
-            System.out.println(e.getErrorDescription());
-            return null;
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return null;
-        }
+    // constructor
+    public PixService() {
     }
 
     /**
@@ -66,6 +33,8 @@ public class PixService {
      *         Gerencianet API.
      */
     public List<String> generateCharge(String valor) throws FileNotFoundException {
+        // transform the value to string
+
         String campo1 = "Informação Adicional1 do PSP-Recebedor";
         String campo2 = "Informação Adicional3 do PSP-Recebedor";
         String cpf = "04145605403";
@@ -84,7 +53,7 @@ public class PixService {
         JSONObject body = new JSONObject();
         body.put("calendario", new JSONObject().put("expiracao", 3600));
         body.put("devedor", new JSONObject().put("cpf", cpf).put("nome", nome));
-        body.put("valor", new JSONObject().put("original", String.valueOf(valor)));
+        body.put("valor", new JSONObject().put("original", valor));
         body.put("chave", "acbf2098-fc24-4e9d-ace8-6b99fd5dee24");
         body.put("solicitacaoPagador", solicitacaoPagador);
 
@@ -151,7 +120,7 @@ public class PixService {
     public static void main(String[] args) throws FileNotFoundException {
         PixService pixService = new PixService();
 
-        System.out.println(pixService.generateCharge(0.01));
+        // System.out.println(pixService.generateCharge(0.01));
 
         // call the method for generate QRCode
         System.out.println(pixService.generateQRCode("17"));
